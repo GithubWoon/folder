04c977ea809b6020b08c2e75eb102711780a3043
GithubWoon <eo2458@naver.com>
2023-07-24 17:50:59 +0900

    0724

---
+++ 230724/모달창.js
@@ -0,0 +1,7 @@
+document.getElementById("open").onclick = function() {
+    document.getElementById("modal").style.display="block";
+}
+
+document.getElementById("close").onclick = function() {
+    document.getElementById("modal").style.display="none";
+}   
